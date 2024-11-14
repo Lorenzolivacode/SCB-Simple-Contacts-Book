5e25778043c04a6cc4cbe037e03bedf61c7ef9f8
Lorenzolivacode <lorenzo.oliva.1308@gmail.com>
2024-11-14 19:50:37 +0100

    fix: resolve promise in layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -17,15 +17,16 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
-  params: { locale },
+  params,
 }: Readonly<{
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }>) {
+  const { locale } = await params;
   const messages = await getMessages({ locale });
 
   return (
-    <html lang="it">
+    <html lang={locale}>
       <body>
         <NextIntlClientProvider messages={messages}>
           <ThemeProvider>
